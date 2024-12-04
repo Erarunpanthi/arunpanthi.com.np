f871e8d804cbbbe830c4f119e2fb0470776dccbe
Erarunpanthi <erarunpanthi@gmail.com>
2024-12-04 15:41:53 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+document.getElementById("detailsForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    
 // Form and Page Handling
 const form = document.getElementById("detailsForm");
 const userForm = document.getElementById("user-form");

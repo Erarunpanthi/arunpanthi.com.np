2c02d49bbee13742781e3c132f49a5ce40a8af38
Erarunpanthi <erarunpanthi@gmail.com>
2026-04-16 21:49:53 +0545

    Auto upload all files and folders

--- js/layout.js
+++ js/layout.js
@@ -289,30 +289,6 @@
       }
     });
   }
-// layout.js
-(function () {
-  "use strict";
-
-  var WATERMARK_LOGO = "https://arunpanthi.com.np/Photos/logo.png";
-
-  function addWatermark() {
-    var wm = document.createElement("div");
-    wm.className = "watermark";
-    wm.style.position = "fixed";
-    wm.style.top = 0;
-    wm.style.left = 0;
-    wm.style.width = "100%";
-    wm.style.height = "100%";
-    wm.style.pointerEvents = "none";
-    wm.style.background = "url('" + WATERMARK_LOGO + "') center center no-repeat";
-    wm.style.backgroundSize = "200px";
-    wm.style.opacity = "0.1";
-    wm.style.zIndex = "9999";
-    document.body.appendChild(wm);
-  }
-
-  document.addEventListener("DOMContentLoaded", addWatermark);
-})();
 
 
   // Initialization
